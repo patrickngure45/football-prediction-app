a0cc370d2758227383c4ac8312bd9b7a7f49cce0
patrickngure45 <ngurengure10@gmail.com>
2021-11-15 19:01:03 +0300

    removed react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Route } from 'react-router-dom';
 import App from './App';
 
 import './index.css';
 
 ReactDOM.render(
-  <Route>
-    <App />
-  </Route>,
+  <App />,
+
   document.getElementById('root')
 );
